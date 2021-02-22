94b775f1e9eb6ec251a8f64da636df38274c1194
anggifergian <anggyfergian56@gmail.com>
2021-02-23 01:05:33 +0700

    Add axiata component

---
+++ src/app/axiata/axiata.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-axiata',
+    templateUrl: './axiata.component.html',
+    styleUrls: ['./axiata.component.scss']
+})
+export class AxiataComponent {}
