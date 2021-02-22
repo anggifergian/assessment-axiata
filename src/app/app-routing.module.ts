745bd2f886675d49cec935130d2b4468c8bf9a67
anggifergian <anggyfergian56@gmail.com>
2021-02-23 00:06:06 +0700

    Add Material Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OnboardingComponent } from "./onboarding/onboarding.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'onboarding', component: OnboardingComponent },
+  { path: '', redirectTo: '/onboarding', pathMatch: 'full' },
+  { path: '**', redirectTo: '/onboarding', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
