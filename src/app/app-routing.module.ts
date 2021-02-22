d80b07470395a29203f5d0419725d9a41d9e582e
anggifergian <anggyfergian56@gmail.com>
2021-02-23 01:06:14 +0700

    Add axiata routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { OnboardingComponent } from "./onboarding/onboarding.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: 'onboarding', pathMatch: 'full' },
   { path: 'onboarding', component: OnboardingComponent },
-  { path: '', redirectTo: '/onboarding', pathMatch: 'full' },
-  { path: '**', redirectTo: '/onboarding', pathMatch: 'full' }
+  { path: 'axiata', loadChildren: './axiata/axiata.module#AxiataModule' },
+  { path: '**', redirectTo: 'onboarding', pathMatch: 'full' }
 ];
 
 @NgModule({
