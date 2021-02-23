ed7e6bbb10b886f680164b6765396452292be52f
anggifergian <anggyfergian56@gmail.com>
2021-02-23 08:09:56 +0700

    Fixing axiata container ui

--- src/app/axiata/axiata.component.ts
+++ src/app/axiata/axiata.component.ts
@@ -1,8 +1,18 @@
 import { Component } from "@angular/core";
+import { onMainContentChange, onSideNavChange, animateText } from "../animations/animations";
 
 @Component({
     selector: 'app-axiata',
     templateUrl: './axiata.component.html',
-    styleUrls: ['./axiata.component.scss']
+    styleUrls: ['./axiata.component.scss'],
+    animations: [onSideNavChange, onMainContentChange, animateText]
 })
-export class AxiataComponent {}
+export class AxiataComponent {
+    public sideNavState: boolean = false;
+    public linkText: boolean = false;
+
+    onSinenavToggle() {
+        this.sideNavState = !this.sideNavState;
+        this.linkText = this.sideNavState;
+    }
+}
