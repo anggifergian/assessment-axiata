522d547e1e01e8734de0e993e213196870df0e58
anggifergian <anggyfergian56@gmail.com>
2021-02-23 08:47:25 +0700

    Fixing animate

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -15,6 +15,5 @@ export const onMainContentChange = trigger('onMainContentChange', [
 export const animateText = trigger('animateText', [
     state('hide', style({ 'display': 'none', opacity: 0,})),
     state('show', style({ 'display': 'block', opacity: 1,})),
-    transition('close => open', animate('350ms ease-in')),
-    transition('open => close', animate('200ms ease-out')),
+    transition('hide => show', animate('0ms 5s ease')),
 ]);
