b974ad155f7f23a8f373a0c1e9348836dc4862dc
anggifergian <anggyfergian56@gmail.com>
2021-02-23 14:43:47 +0700

    Add ticket dummy data

---
+++ src/app/services/ticket.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TicketService {
+
+    constructor(private http: HttpClient) {}
+
+    public getTickets(): Observable<any> {
+        return this.http.get("/assets/ticket.json");
+    }
+}
