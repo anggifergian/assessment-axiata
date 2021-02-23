ea592b3312c1689ff635c8e7e08fafadfb70883f
anggifergian <anggyfergian56@gmail.com>
2021-02-23 14:44:27 +0700

    Inject http module and adjust route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { TicketService } from './services/ticket.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,10 +12,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
 
 import { OnboardingComponent } from "./onboarding/onboarding.component";
 import { AxiataComponent } from './axiata/axiata.component';
 import { AllticketComponent } from './allticket/allticket.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -25,13 +32,20 @@ import { AllticketComponent } from './allticket/allticket.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    RouterModule,
+    HttpClientModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatSelectModule,
+    FormsModule,
+    TableModule
   ],
-  providers: [],
+  providers: [
+    TicketService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/axiata/axiata.module.ts
+++ src/app/axiata/axiata.module.ts
@@ -4,20 +4,20 @@ import { AllticketComponent } from '../allticket/allticket.component';
 import { AxiataComponent } from './axiata.component';
 
 const axiataRoutes: Routes = [
-    {
-        path: '',
-        component: AxiataComponent,
-        children: [
-            { path: '', redirectTo: 'allticket', pathMatch: 'full'},
-            { path: 'allticket', component: AllticketComponent},
-            { path: '**', redirectTo: '' }
-        ]
-    },
+  {
+    path: '',
+    component: AxiataComponent,
+    children: [
+      { path: '', redirectTo: 'allticket', pathMatch: 'full'},
+      { path: 'allticket', component: AllticketComponent},
+      { path: '**', redirectTo: 'allticket', pathMatch: 'full' }
+    ]
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(axiataRoutes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(axiataRoutes)],
+  exports: [RouterModule]
 })
 export class AxiataModule {
 
