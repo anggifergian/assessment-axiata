2ce5a8819e6e43eebad34e84190e572db60af835
anggifergian <anggyfergian56@gmail.com>
2021-02-23 08:09:32 +0700

    Add animateText

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -7,7 +7,14 @@ export const onSideNavChange = trigger('onSideNavChange', [
 ]);
 
 export const onMainContentChange = trigger('onMainContentChange', [
-    state('close', style({'margin-left':'62px'})),
+    state('close', style({'margin-left':'57px'})),
     state('open', style({'margin-left':'200px'})),
     transition('close <=> open', animate('250ms ease-in'))
+]);
+
+export const animateText = trigger('animateText', [
+    state('hide', style({ 'display': 'none', opacity: 0,})),
+    state('show', style({ 'display': 'block', opacity: 1,})),
+    transition('close => open', animate('350ms ease-in')),
+    transition('open => close', animate('200ms ease-out')),
 ]);
