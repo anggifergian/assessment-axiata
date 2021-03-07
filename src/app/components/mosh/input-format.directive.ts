db0d3e73822a1f40f95a3d45306c59ac8983c3af
anggifergian <anggyfergian56@gmail.com>
2021-03-08 05:21:26 +0700

    Add Input Format Directive

--- src/app/components/mosh/input-format.directive.ts
+++ src/app/components/mosh/input-format.directive.ts
@@ -1,8 +1,27 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
+  @Input('appInputFormat') format;
 
+  constructor(private el: ElementRef) {}
+
+  @HostListener('blur') onBlur() {
+    let value: string = this.el.nativeElement.value;
+    let words = value.split(" ");
+
+    for (let i = 0; i < words.length; i++)
+      words[i] = this.toTitleCase(words[i]);
+
+    value = words.join(" ");
+    console.log(value);
+
+    this.el.nativeElement.value = value;
+  }
+
+  toTitleCase(word: string) {
+    return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+  }
 }
