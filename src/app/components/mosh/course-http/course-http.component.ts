61284c16cf47489a213a0b04f1849f9cdad54f7e
anggifergian <anggyfergian56@gmail.com>
2021-03-14 08:22:47 +0700

    Implement Optimistic updates

--- src/app/components/mosh/course-http/course-http.component.ts
+++ src/app/components/mosh/course-http/course-http.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppError } from '../services/app-error';
+import { BadInput } from '../services/bad-input';
+import { NotFoundError } from '../services/not-found-error';
 import { PostService } from '../services/post.service';
 
 @Component({
@@ -7,36 +10,53 @@ import { PostService } from '../services/post.service';
   styleUrls: ['./course-http.component.scss']
 })
 export class CourseHttpComponent implements OnInit {
-  posts: any[];
+  posts;
 
   constructor(private postService: PostService) { }
 
   ngOnInit(): void {
-    this.postService.getPosts()
-      .subscribe(posts => this.posts = posts);
+    this.postService.getAll()
+      .subscribe(resp => this.posts = resp);
   }
 
   createPost(input: HTMLInputElement) {
     const post = { title: input.value };
+    this.posts.splice(0, 0, post);
+
     input.value = '';
 
-    this.postService.createPost(post)
-      .subscribe((response:any) => {
-        post["id"] = response.id;
-        this.posts.splice(0, 0, post);
-      });
+    this.postService.create(post)
+      .subscribe(
+        (response:any) => {
+          post["id"] = response.id;
+        },
+        (error: AppError) => {
+          this.posts.splice(0, 1);
+
+          if (error instanceof BadInput)
+            console.log(error.originalError);
+          else throw error;
+        });
   }
 
   updatePost(post) {
-    this.postService.updatePost(post.id, {isRead: true})
+    this.postService.update(post, {isRead: true})
       .subscribe(resp => console.log(resp));
   }
 
   deletePost(post) {
-    this.postService.deletePost(post.id)
-      .subscribe(resp => {
-        let index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-      });
+    let index = this.posts.indexOf(post);
+    this.posts.splice(index, 1);
+
+    this.postService.delete(post.id)
+      .subscribe(
+        null,
+        (error: AppError) => {
+          this.posts.splice(index, 0, post);
+          if (error instanceof NotFoundError)
+            alert("This post has been deleted.");
+          else throw error;
+        }
+      );
   }
 }
